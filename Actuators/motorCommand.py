b4bef1a581841e6c2eabac64c38fc16117b80e1c
ischmidt707 <ischmidt707@gmail.com>
2021-10-07 21:38:06 -0600

    3 core folders made

---
+++ Actuators/motorCommand.py
@@ -0,0 +1,4 @@
+"""
+This is the class that manages all commands to
+the arduino for movement.
+"""
---
+++ runRobot.py
@@ -0,0 +1,18 @@
+"""
+---- MSU RoboCats ----
+
+This is the file from which everything on the
+submarine is ran/tested.
+
+"""
+
+class Main:
+
+    def __init__(self):
+        pass
+
+    def main(self):
+        print("TODO: prompt for run mode and execute accordingly")
+
+m = Main()
+m.main()
