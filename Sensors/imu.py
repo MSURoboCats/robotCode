bbda2b2dc2e5c1e16d0ce78a5af2a8c42497c7bf
ischmidt707 <ischmidt707@gmail.com>
2021-10-07 21:48:11 -0600

    Blank py files added

---
+++ Sensors/imu.py
@@ -0,0 +1,4 @@
+"""
+Collecting and returning data from the IMU
+- Intertial Measurement Unit
+"""
---
+++ runModeScripts/autonomous.py
@@ -0,0 +1,12 @@
+"""
+Official and current class file for autonomous
+control algorithm
+"""
+
+class Auto:
+
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
