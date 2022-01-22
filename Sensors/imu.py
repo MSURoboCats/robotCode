1fc7b66196a461f455b1cea4c78c9ed45c2e687a
W. Ward <woward00@gmail.com>
2022-01-21 18:44:15 -0700

    convert IMU code into an IMU class

--- Sensors/imu.py
+++ Sensors/imu.py
@@ -1,83 +1,83 @@
-"""
-Collecting and returning data from the IMU
-- Intertial Measurement Unit
-"""
-#This is the simplified IMU code. The main function shows how to send data requests. Built for the TyphoonII Spartion GEDC-8 IMU -JG
-#Butter is the scipy package that allows most of the calculations to be done -ZW
+import serial
 import numpy as np
-from scipy.signal import butter,filtfilt# Filter requirements.
-import sys, math, time, serial, re, numpy as np
-#port = '/dev/ttyUSB0' #Intel Nuc
-port = 'COM5'   #Josh PC
+import re
+import time
 
-baud = 115200
-fs = 100.0  #Sample rate, Hz
-T = 1/fs
-cutoff = fs/2   #Desired cutoff frequency of the filter, Hz,
-                #Sightly higher than actual 1.2 Hznyq = 0.5 * fs
-                #Nyquist Frequencyorder = 2
-                #Sin wave can be approx represented as quadratic ##JG-?
-nyq = 0.5*fs
-n = int(T * fs) #Total number of samples
 
-ser = serial.Serial(port, baud)
-ser.flush() #Flushes the store data from serial
-print("Ready!")
+class ImuAhrsSparton:
 
-def get_imu_data(command):
-    global ser
+    def __init__(self, *, port: str = "COM5", baud_rate: int = 115200):
+        self.name: str = "AHRS Sparton IMU"
+        self.port: str = port  # COM5 on Josh PC
+        self.baud_rate: int = baud_rate
+        self.fs_sample_rate: float = 100.0  # in Hz
+        self.T_period: float = 1 / self.fs_sample_rate
+        self.cutoff_frequency: float = self.fs_sample_rate / 2  # Desired cutoff frequency of the filter, Hz, Sightly higher than actual 1.2 Hznyq = 0.5 * fs
+        self.nyq_nyquist_frequency: float = 0.5 * self.fs_sample_rate  # Nyquist Frequencyorder = 2, Sin wave can be approx represented as quadratic ##JG-?
+        self.n_number_of_samples: int = int(self.T_period * self.fs_sample_rate)  # Total number of samples
+        self.ser = serial.Serial(self.port, self.baud_rate)
+        self.ser.flush()
 
-    ser.write(command.encode())
-    data = ser.readline().decode('utf-8')
-    values = np.array(re.findall('([-\d.]+)', data)).astype(float)
-    return values
+    def get_imu_data(self, command):
+        self.ser.write(command.encode())
+        data = self.ser.readline().decode('utf-8')
+        values = np.array(re.findall('([-\d.]+)', data)).astype(float)
+        return values
 
-def get_dist_north():
-    print("Getting the heading away from north!")
-    #Call the get data func at data point $PSPA,QUAT\r\n
-    #See manual for locations of data
-    #This will return a float from -1 to 1 
-    magnetometer = get_imu_data("$PSPA,QUAT\r\n")
-    #Make it the true hedding by multiplying by 180
-    hedding = ((magnetometer[0])*180)
-    print(magnetometer[0])
-    #Returns deg off of north
-    return hedding
+    def get_dist_north(self):
+        print("Getting the heading away from north!")
+        # Call the get data func at data point $PSPA,QUAT\r\n
+        # See manual for locations of data
+        # This will return a float from -1 to 1
+        magnetometer = self.get_imu_data("$PSPA,QUAT\r\n")
+        # Make it the true hedding by multiplying by 180
+        heading = ((magnetometer[0]) * 180)
+        print(magnetometer[0])
+        # Returns deg off of north
+        return heading
 
-def get_hedding():
-    print("Getting the true heading!")
-    #Call the get data func at data point $PSPA,QUAT\r\n
-    #See manual for locations of data
-    #This will return a float from -1 to 1 
-    magnetometer = get_imu_data("$PSPA,QUAT\r\n")
-    #Make it the true hedding by multiplying by 180
-    hedding = ((magnetometer[0]+1)*180)
-    print(magnetometer[0])
-    #Returns deg off of north
-    return hedding
+    def get_heading(self):
+        print("Getting the true heading!")
+        # Call the get data func at data point $PSPA,QUAT\r\n
+        # See manual for locations of data
+        # This will return a float from -1 to 1
+        magnetometer = self.get_imu_data("$PSPA,QUAT\r\n")
+        # Make it the true heading by multiplying by 180
+        heading = ((magnetometer[0] + 1) * 180)
+        print(magnetometer[0])
+        # Returns deg off of north
+        return heading
 
-def get_pitch():
-    print("Getting the pitch!")
-    #Call the get data func at data point $PSP
-    #See manual for locations of data
-    #This will return a pitch, roll, 
-    pitch = get_imu_data("$PSPA,PR\n")
-    print(pitch[1])
-    #Returns deg off of level
-    return pitch
+    def get_pitch(self):
+        print("Getting the pitch!")
+        # Call the get data func at data point $PSP
+        # See manual for locations of data
+        # This will return a pitch, roll,
+        pitch = self.get_imu_data("$PSPA,PR\n")
+        print(pitch[1])
+        # Returns deg off of level
+        return pitch
 
-def get_roll():
-    #Not currently implimented
-    t=0
-    return t 
+    def get_roll(self):
+        # Not currently implemented
+        t = 0
+        return t
+
+    def test_pitch(self) -> None:
+        """
+        Gets the pitch according to the TyphoonII IMU for testing sensor
+        """
+        print("Main run!")
+        print(self.get_pitch())
+        time.sleep(5)
+        print(self.get_pitch())
+        time.sleep(5)
+        print(self.get_pitch())
+        time.sleep(5)
+        print("Done!")
+
 
 if __name__ == "__main__":
-    #This code calls for the pitch according to the TyphoonII IMU for testing sensor
-    print("Main run!")
-    print(get_pitch())
-    time.sleep(5)
-    print(get_pitch())
-    time.sleep(5)
-    print(get_pitch())
-    time.sleep(5)
-    print("Done!")
+    imu: ImuAhrsSparton = ImuAhrsSparton()
+    imu.test_pitch()
+
---
+++ Sensors/imu_functional.py
@@ -0,0 +1,91 @@
+"""
+Collecting and returning data from the IMU
+- Inertial Measurement Unit
+"""
+# This is the simplified IMU code. The main function shows how to send data requests. Built for the TyphoonII Spartion GEDC-8 IMU -JG
+# Butter is the scipy package that allows most of the calculations to be done -ZW
+import numpy as np
+import re
+import serial
+import time
+
+# port = '/dev/ttyUSB0' #Intel Nuc
+port = 'COM5'  # Josh PC
+
+baud = 115200
+fs = 100.0  # Sample rate, Hz
+T = 1 / fs
+cutoff = fs / 2  # Desired cutoff frequency of the filter, Hz,
+# Sightly higher than actual 1.2 Hznyq = 0.5 * fs
+# Nyquist Frequencyorder = 2
+# Sin wave can be approx represented as quadratic ##JG-?
+nyq = 0.5 * fs
+n = int(T * fs)  # Total number of samples
+
+ser = serial.Serial(port, baud)
+ser.flush()  # Flushes the store data from serial
+print("Ready!")
+
+
+def get_imu_data(command):
+    global ser
+
+    ser.write(command.encode())
+    data = ser.readline().decode('utf-8')
+    values = np.array(re.findall('([-\d.]+)', data)).astype(float)
+    return values
+
+
+def get_dist_north():
+    print("Getting the heading away from north!")
+    # Call the get data func at data point $PSPA,QUAT\r\n
+    # See manual for locations of data
+    # This will return a float from -1 to 1
+    magnetometer = get_imu_data("$PSPA,QUAT\r\n")
+    # Make it the true hedding by multiplying by 180
+    hedding = ((magnetometer[0]) * 180)
+    print(magnetometer[0])
+    # Returns deg off of north
+    return hedding
+
+
+def get_hedding():
+    print("Getting the true heading!")
+    # Call the get data func at data point $PSPA,QUAT\r\n
+    # See manual for locations of data
+    # This will return a float from -1 to 1
+    magnetometer = get_imu_data("$PSPA,QUAT\r\n")
+    # Make it the true hedding by multiplying by 180
+    hedding = ((magnetometer[0] + 1) * 180)
+    print(magnetometer[0])
+    # Returns deg off of north
+    return hedding
+
+
+def get_pitch():
+    print("Getting the pitch!")
+    # Call the get data func at data point $PSP
+    # See manual for locations of data
+    # This will return a pitch, roll,
+    pitch = get_imu_data("$PSPA,PR\n")
+    print(pitch[1])
+    # Returns deg off of level
+    return pitch
+
+
+def get_roll():
+    # Not currently implemented
+    t = 0
+    return t
+
+
+if __name__ == "__main__":
+    # This code calls for the pitch according to the TyphoonII IMU for testing sensor
+    print("Main run!")
+    print(get_pitch())
+    time.sleep(5)
+    print(get_pitch())
+    time.sleep(5)
+    print(get_pitch())
+    time.sleep(5)
+    print("Done!")
