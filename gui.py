b1d7fc156e28f4166c79870ade0f2d220111b073
W. Ward <woward00@gmail.com>
2022-01-24 23:56:38 -0700

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,4 +1,7 @@
+import sys
+
 import PyQt5  # use this for gui
+from PyQt5.QtWidgets import QApplication, QMainWindow
 
 from static_utilities import StaticUtilities
 
@@ -6,5 +9,22 @@ from static_utilities import StaticUtilities
 class GUI:
 
     def __init__(self) -> None:
+        self.app = QApplication(sys.argv)
+        self.resolution = self.app.desktop().screenGeometry()
+        self.width, self.height = self.resolution.width(), self.resolution.height()
+        self.window_width = self.width/2
+        self.window_height = self.height/2
+        self.window = QMainWindow()
+        self.window.setGeometry(int(self.window_width/2), int(self.window_height/2), int(self.window_width), int(self.window_height))
+        self.window.setWindowTitle("Robocats Testing GUI")
         StaticUtilities.logger.info(f"GUI initialized")
+
+    def gui(self) -> None:
+        self.window.show()
+        sys.exit(self.app.exec_())
+
+
+if __name__ == "__main__":
+    gui = GUI()
+    gui.gui()
 
