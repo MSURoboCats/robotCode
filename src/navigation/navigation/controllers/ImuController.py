bbc1cc34c015ef4526b7adde6e4be1aa13b9a0ed
Treser Streib <treserstreib@outlook.com>
2023-03-28 18:50:33 -0600

    Imu controller node draft

--- src/navigation/navigation/controllers/ImuController.py
+++ src/navigation/navigation/controllers/ImuController.py
@@ -2,35 +2,50 @@ import rclpy
 from rclpy.node import Node
 
 # Import interface definitions
-from interfaces.msg import ThrusterCommand
+from interfaces.msg import ImuOutput
+from interfaces.msg import ImuRawData
 
-class ThrusterController(Node):
-    def __init__(self):
-        super().__init__("thruster_controller")
+class ImuController(Node): #create ImuController class from node class
+    def __init__(self):     #create instance of ImuController class
+        super().__init__("Imu_controller")   #inherit attributes from parent class
 
-        # Instantiate a new subscriber that is listening for thruster commands on the 'thruster' topic
+        # Instantiate a new subscriber that is listening for imu outputs on the 'Imu_raw_data' topic
         self.create_subscription = self.create_subscription(
-            ThrusterCommand,
-            "thruster",
+            ImuRawData,
+            "Imu_raw_data",
             self._handle_message 
         )
-    
-    def _handle_message(self):
-        # This is where the thruster command will be handled (i.e., converted into native instructionts that can be interpreted by the Blue Robotics T100 thrusters)
-        # TODO: Implement thruster control logic
 
+    def _handle_message(self):
+        # This is where the raw data will be processed
+        # TODO: initial data processing
         # TODO: Add context to log message
-        self.get_logger().info("Actating thruster")
+        self.get_logger().info("Processing information from IMU")
+
+
+        #instantiate new publisher that publishes processed data to "Imu_output" topic
+    def _send_imu_info(self):
+        publisher = self.create_publisher(ImuOutput, "Imu_output")
+        # TODO: format data and assign it to the ProcessedData variable
+        ProcessedData = 0
+
+        # Publish command to 'Imu_output' topic
+        publisher.publish(ProcessedData)       
+
+        # TODO: Add context to logging message
+        self.get_logger().info("Sending IMU information to Navigation Node")
+
 
 def main(args=None):
-    # Consume parameters
+    # Initialize the node and consume any paramters
     rclpy.init(args=args)
 
-    # Initalize thruster controller
-    thruster_contoller = ThrusterController()
+    # Perform any configuration
+    Imu_controller = ImuController()
 
-    # Release control of node to ROS2
-    rclpy.spin(thruster_contoller)
+    # Release control of the node to the ROS network
+    rclpy.spin(Imu_controller)
 
 if __name__ == "__main__":
     main()
+    
