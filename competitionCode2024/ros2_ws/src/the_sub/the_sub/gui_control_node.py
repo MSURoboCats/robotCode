d389806e189ff79284894fa55889003ecfd02a80
David Jensen <dhjensen02@gmail.com>
2024-07-11 16:05:13 -0600

    Updated GUI motor mappings page

--- competitionCode2024/ros2_ws/src/the_sub/the_sub/gui_control_node.py
+++ competitionCode2024/ros2_ws/src/the_sub/the_sub/gui_control_node.py
@@ -1,10 +1,11 @@
 import rclpy
 from rclpy.node import Node
 
+from interfaces.msg import ControlData, HullData
+
+from std_msgs.msg import String
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import BatteryState
-
-from interfaces.msg import ControlData, HullData
 
 import pygame
 import tkinter as tk
@@ -12,6 +13,9 @@ from tkinter import ttk, filedialog
 from PIL import ImageTk, Image
 from threading import Thread
 import time
+
+from cv_bridge import CvBridge
+import cv2
 
 global vel
 vel = 1.0
@@ -91,6 +95,17 @@ class button():
                 self.off()
         self.btn.configure(bg=self.back[self.idx])
 
+class MotorChooser():
+    def __init__(self, tab, motor_num, row, col_start):
+       label_text = "Motor "+str(motor_num)+")    "
+       ttk.Label(tab, text =label_text, font = "Helvetica 10 bold").grid(column = col_start, row = row)
+       ESCs = ["1", "2", "3","4","5","6","7","8"]; directions = ["Forward","Reverse"]
+       ESC = tk.StringVar(tab);ESC.set("Choose ESC"); direction = tk.StringVar(tab); direction.set("Choose Direction")
+       drop_ESC = tk.OptionMenu(tab ,ESC , *ESCs ); drop_dir = tk.OptionMenu(tab, direction, *directions)
+       
+       ttk.Label(tab, text ="ESC: ").grid(column = col_start+1, row = row);drop_ESC.grid(column = col_start+2, row=row)
+       ttk.Label(tab, text ="Direction: ").grid(column = col_start+3, row = row);drop_dir.grid(column = col_start+4, row =row)
+
 class GUI():
     def __init__(self):
         #makes tabs and sets up titles
@@ -110,17 +125,32 @@ class GUI():
         self.root.mainloop()
 
     def motorMappings(self):
-        #still having a little trouble with the image so this is incomplete for right now
-        MM = ttk.Frame(self.SubControlNode)
+       
+        MM = tk.Frame(self.SubControlNode)
+       
         self.SubControlNode.add(MM, text ='Motor Mappings')
-        ttk.Label(MM, text ="Motors: ").grid(column = 0, row = 0,  **self.paddings)
-        """
-        imgDIR = os.getcwd()+"\SUB-PICTURE.png"
-        canvas = tk.Canvas(MM, width=600, height=400, bg='white')
-        canvas.grid(column = 1, row = 1,  **self.paddings)
-        python_image = tk.PhotoImage(master = MM,file=imgDIR)
-        canvas.create_image((600, 400),image=python_image)
-        """
+        ttk.Label(MM, text ="Motor Mappings: ", font = "Helvetica 12 bold").grid(column = 0, row = 0,  **self.paddings)
+
+        #image showing
+        image = Image.open("SUB-PICTURE.png")
+        canvas_for_image = tk.Canvas(MM, bg='green', height=348, width=438, borderwidth=0, highlightthickness=0)
+        canvas_for_image.grid(row=1, column=0, sticky='nesw', padx=0, pady=0,rowspan = 16)
+        canvas_for_image.image = ImageTk.PhotoImage(image.resize((438, 348)))
+        canvas_for_image.create_image(0, 0, image=canvas_for_image.image, anchor='nw')
+       
+        self.M1 = MotorChooser(MM,1,1,1)
+        self.M2 = MotorChooser(MM,2,3,1)
+        self.M3 = MotorChooser(MM,3,5,1)
+        self.M4 = MotorChooser(MM,4,7,1)
+        self.M5 = MotorChooser(MM,5,9,1)
+        self.M6 = MotorChooser(MM,6,11,1)
+        self.M7 = MotorChooser(MM,7,13,1)
+        self.M8 = MotorChooser(MM,8,15,1)
+       
+        ttk.Label(MM, text ="Test ESC: ").grid(column = 2, row = 17,  **self.paddings)
+        ESCs = ["1", "2", "3","4","5","6","7","8"]; ESC = tk.StringVar(MM);ESC.set("Choose ESC");
+        drop_ESC = tk.OptionMenu(MM ,ESC , *ESCs );drop_ESC.grid(column = 3, row=17)
+        button(MM,"Test", {'row':17,'column':4}, EX_button_off,EX_button_on,"grey","green")
    
     #all of the options
     def options(self):
@@ -133,25 +163,35 @@ class GUI():
         ttk.Label(Options, text ="Cameras: ").grid(column = 1, row = 1,  **self.paddings)
         down = button(Options,"DOWN", {'row':1,'column':2}, EX_button_off,EX_button_on,"grey","green")
         forward = button(Options,"FORWARD", {'row':1,'column':3}, EX_button_off,EX_button_on,"grey","green")
-        stereo = button(Options,"STEREO", {'row':1,'column':4}, EX_button_off,EX_button_on,"grey","green")
+        #stereo = button(Options,"STEREO", {'row':1,'column':4}, EX_button_off,EX_button_on,"grey","green")
 
        
         #image collection file path chooser
         ttk.Label(Options, text ="Path To Save: ").grid(column = 1, row = 2,  **self.paddings)
         self.path_entry = ttk.Entry(Options, textvariable=self.savePath)
+        self.path_entry.insert(0, '/home/robocats/Desktop/developmentEnvironment/robotCode/competitionCode2024/ros2_ws/training_data')
         self.path_entry.grid(column = 2,row =2, **self.paddings)
         button(Options,"Search Path", {'row':2,'column':3}, self.browsefunc,self.browsefunc,"grey","grey")
-       
-        #Image collection saving interval
-        ttk.Label(Options, text ="Saving Interval: ").grid(column = 1, row = 3,  **self.paddings)
-        self.interval = tk.StringVar()
-        intervalTextbox = ttk.Entry(Options, textvariable=self.interval)
+
+        #Image collection folder name
+        ttk.Label(Options, text ="Folder name: ").grid(column = 1, row = 3,  **self.paddings)
+        self.training_data_folder_name = tk.StringVar()
+        intervalTextbox = ttk.Entry(Options, textvariable=self.training_data_folder_name)
+        intervalTextbox.insert(0, 'test1')
         intervalTextbox.grid(column = 2, row = 3, **self.paddings)
         intervalTextbox.focus()
-       
+
+        #Image collection saving interval
+        ttk.Label(Options, text ="Saving Interval (seconds): ").grid(column = 1, row = 4,  **self.paddings)
+        self.interval = tk.StringVar()
+        intervalTextbox = ttk.Entry(Options, textvariable=self.interval)
+        intervalTextbox.insert(0, '1')
+        intervalTextbox.grid(column = 2, row = 4, **self.paddings)
+        intervalTextbox.focus()
+        
         #live preview has drop down menu and a button to launch it
-        ttk.Label(Options, text ="Live Preview: ").grid(column = 0, row = 4,  **self.paddings)
-        cameras = ["cam1", "cam2", "cam3"]; CVModels = ["mod1","mod2"]
+        ttk.Label(Options, text ="Live Preview: ").grid(column = 0, row = 5,  **self.paddings)
+        cameras = ["FORWARD", "DOWN", ""]; CVModels = ["pool_tuned_base","dry_buoy"]
         cam = tk.StringVar(Options);cam.set("Choose Camera"); mod = tk.StringVar(Options); mod.set("Choose CV Model")
         drop_cam = tk.OptionMenu( Options ,cam , *cameras ); drop_mod = tk.OptionMenu(Options, mod, *CVModels)
         drop_cam.grid(column = 1, row=5); drop_mod.grid(column = 2, row =5)
@@ -167,31 +207,33 @@ class GUI():
         self.lin_acc= tk.StringVar()
         #how you can set variables ex) self.battery_level.set(str(90)))
         #this just shows the status
-        ttk.Label(Options, text ="Sub status: ").grid(column = 0, row = 6,  **self.paddings)
-        ttk.Label(Options, text = "Battery Level:").grid(column = 1, row = 7,  **self.paddings);ttk.Label(Options, textvariable = self.battery_level).grid(column = 2, row = 7,  **self.paddings)
-        ttk.Label(Options, text = "Tempature:").grid(column = 3, row = 7,  **self.paddings);ttk.Label(Options, textvariable = self.temp).grid(column = 4, row = 7,  **self.paddings)
-        ttk.Label(Options, text = "Pressure:").grid(column = 1, row = 8,  **self.paddings);ttk.Label(Options, textvariable = self.pressure).grid(column = 2, row = 8,  **self.paddings)
-        ttk.Label(Options, text = "Humidity:").grid(column = 3, row = 8,  **self.paddings);ttk.Label(Options, textvariable = self.humidity).grid(column = 4, row = 8,  **self.paddings)
-        ttk.Label(Options, text = "Depth:").grid(column = 1, row = 9,  **self.paddings);ttk.Label(Options, textvariable = self.depth).grid(column = 2, row = 9,  **self.paddings)
-        ttk.Label(Options, text = "Rot:").grid(column = 3, row = 9,  **self.paddings);ttk.Label(Options, textvariable = self.rot).grid(column = 4, row = 9,  **self.paddings)
-        ttk.Label(Options, text = "Angular Velocity:").grid(column = 1, row = 10,  **self.paddings);ttk.Label(Options, textvariable = self.rot_vel).grid(column = 2, row = 10,  **self.paddings)
-        ttk.Label(Options, text = "Linear Acceleration:").grid(column = 3, row = 10,  **self.paddings);ttk.Label(Options, textvariable = self.lin_acc).grid(column = 4, row = 10,  **self.paddings)
+        ttk.Label(Options, text ="Sub status: ").grid(column = 0, row = 7,  **self.paddings)
+        ttk.Label(Options, text = "Battery Level:").grid(column = 1, row = 8,  **self.paddings);ttk.Label(Options, textvariable = self.battery_level).grid(column = 2, row = 7,  **self.paddings)
+        ttk.Label(Options, text = "Tempature:").grid(column = 3, row = 8,  **self.paddings);ttk.Label(Options, textvariable = self.temp).grid(column = 4, row = 7,  **self.paddings)
+        ttk.Label(Options, text = "Pressure:").grid(column = 1, row = 9,  **self.paddings);ttk.Label(Options, textvariable = self.pressure).grid(column = 2, row = 8,  **self.paddings)
+        ttk.Label(Options, text = "Humidity:").grid(column = 3, row = 9,  **self.paddings);ttk.Label(Options, textvariable = self.humidity).grid(column = 4, row = 8,  **self.paddings)
+        ttk.Label(Options, text = "Depth:").grid(column = 1, row = 10,  **self.paddings);ttk.Label(Options, textvariable = self.depth).grid(column = 2, row = 9,  **self.paddings)
+        ttk.Label(Options, text = "Rot:").grid(column = 3, row = 10,  **self.paddings);ttk.Label(Options, textvariable = self.rot).grid(column = 4, row = 9,  **self.paddings)
+        ttk.Label(Options, text = "Angular Velocity:").grid(column = 1, row = 11,  **self.paddings);ttk.Label(Options, textvariable = self.rot_vel).grid(column = 2, row = 10,  **self.paddings)
+        ttk.Label(Options, text = "Linear Acceleration:").grid(column = 3, row = 11,  **self.paddings);ttk.Label(Options, textvariable = self.lin_acc).grid(column = 4, row = 10,  **self.paddings)
        
         #sub controller
         slider_label = ttk.Label(Options,text='Motor Controler:')
-        slider_label.grid(column=0, row=11,sticky='w')
+        slider_label.grid(column=0, row=12,sticky='w')
         self.current_value = tk.DoubleVar()
         slider = ttk.Scale(Options,from_=0,to=100,orient='horizontal', command=self.slider_changed, variable=self.current_value)
-        slider.grid(column=1,row=12,sticky='we')
+        slider.grid(column=1,row=13,sticky='we')
         current_value_label = ttk.Label( Options,text='Motor power percentage:')
-        current_value_label.grid(row=13, column=1, sticky='n',ipadx=10, ipady=10)
+        current_value_label.grid(row=14, column=1, sticky='n',ipadx=10, ipady=10)
         # value label
         val = self.get_current_value()
         self.value_label = ttk.Label(Options, text=val)
-        self.value_label.grid(row=13,column=2,sticky='n')
+        self.value_label.grid(row=15,column=2,sticky='n')
        
-        controller_btn = button(Options,"Launch Controller", {'row':14,'column':2}, self.open_controller,self.open_controller,"grey","grey")
-       
+        controller_btn = button(Options,"Launch Controller", {'row':15,'column':2}, self.open_controller,self.open_controller,"grey","grey")
+
+        button(Options,"ABORT", {'row':16,'column':0}, EX_button_off,EX_button_off,"red","red")
+
     def open_controller(self):
        
         pygame_controller = controler()
@@ -217,27 +259,70 @@ def GUI_launch():
     gui = GUI()
     gui.start()
 
-class KeyboardController(Node):
+class AllKnowingGUI(Node):
 
     def __init__(self):
-        super().__init__("KeuboardController")
+        super().__init__("all_knowing_gui_node")
 
         # publisher for twists to control the sub
-        #self.twist_pub = self.create_publisher(Twist, "control_twist", 10)
+        self.pub_control_twist = self.create_publisher(
+            Twist,
+            "control_twist",
+            10,
+        )
+
+        # publisher for controlling/starting/stopping frame saver
+        self.pub_frame_saver = self.create_publisher(
+            String,
+            "/frame_saver_commands",
+            10,
+        )
+
 
         # subscriber for control data
-        self.sub_control_data = self.create_subscription(ControlData, '/control_data', self.control_data_callback, 10)
+        self.sub_control_data = self.create_subscription(
+            ControlData,
+            '/control_data',
+            self.control_data_callback,
+            10,
+        )
 
         # subscriber for hull data
-        self.sub_control_data = self.create_subscription(HullData, '/hull_data', self.hull_data_callback, 10)
+        self.sub_control_data = self.create_subscription(
+            HullData,
+            '/hull_data',
+            self.hull_data_callback,
+            10,
+        )
 
         # subscriber for voltage
-        self.voltage = self.create_subscription(BatteryState, "battery_health", self.voltage_callback, 10)
+        self.voltage = self.create_subscription(
+            BatteryState,
+            'battery_health',
+            self.voltage_callback,
+            10,
+        )
 
         # subscriber for forward camera
-
+        self.sub_forward_rgb_camera = self.create_subscription(
+            Image, 
+            '/forward_rgb_camera/video_frames', 
+            self.sub_forward_rgb_camera_callback, 
+            10
+        )
+        
         # subscriber for downward camera
-    
+        self.sub_forward_rgb_camera = self.create_subscription(
+            Image, 
+            '/downard_rgb_camera/video_frames', 
+            self.sub_downward_rgb_camera_callback, 
+            10
+        )
+
+        self.br = CvBridge()
+        self.forward_feed = False
+        self.downward_feed = False
+
     def control_data_callback(self, data: ControlData) -> None:
         gui.depth.set(str(data.depth))
     
@@ -249,6 +334,17 @@ class KeyboardController(Node):
     def voltage_callback(self, data: BatteryState) -> None:
         gui.battery_level.set(str(data.voltage))
 
+    def sub_forward_rgb_camera_callback(self, data: Image) -> None:
+        if self.forward_feed:
+            # convert to cv2 format and display
+            current_frame = self.br.imgmsg_to_cv2(data)
+            cv2.imshow("Forward Feed", current_frame)
+
+    def sub_downward_rgb_camera_callback(self, data: Image) -> None:
+        if self.downward_feed:
+            # convert to cv2 format and display
+            current_frame = self.br.imgmsg_to_cv2(data)
+            cv2.imshow("Forward Feed", current_frame)
 
 
 def spin_listener(args = None):
@@ -273,7 +369,6 @@ def spin_listener(args = None):
 def main(args=None):
     p = Thread(target=GUI_launch)
     p.start()
-    print("got here")
     time.sleep(10)
     spin_listener()
 
