be822ebb89e7a65e33018af9ec7ac7951b7f4071
David Jensen <dhjensen02@gmail.com>
2024-07-22 16:52:18 -0600

    Nothing runs on startup

--- competitionCode2024/ros2_ws/bash_scripts/on_startup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# each of these commands starts up a new terminal tab and runs the commands. Optionally add 'bash' as the last command so the tab will stay open after then final command is completed.
-
-gnome-terminal \
-	--tab --title="hardware" \
-		-- bash -c 'cd /home/robocats/Desktop/developmentEnvironment/robotCode/competitionCode2024/ros2_ws; source install/setup.bash; ros2 launch the_sub hardware_launch.py; '
