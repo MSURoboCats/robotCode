42c200a5a24b9645c35231a0ebeb3c8b65784296
David Jensen <dhjensen02@gmail.com>
2024-08-08 15:22:26 -0600

    updated to save to SD card

--- competitionCode2024/ros2_ws/src/the_sub/the_sub/frame_saver_node.py
+++ competitionCode2024/ros2_ws/src/the_sub/the_sub/frame_saver_node.py
@@ -52,12 +52,13 @@ class FrameSaver(Node):
     self.frame = self.br.imgmsg_to_cv2(data)
   
   def timer_callback(self) -> None:
-    full_img_path = os.path.join(self.file_path, str(self.counter) + '.jpg')
+    # full_img_path = os.path.join(self.file_path, str(self.counter) + '.jpg')
+    full_img_path = os.path.join('/media/robocats/32GB_SD/', str(sys.argv[1]), str(self.counter) + '.jpg')
 
     # ensure that a frame has been received before trying to save images
     if type(self.frame) == np.ndarray:
-      cv2.imwrite(full_img_path, self.frame)
-      self.get_logger().info("Frame saved: %s" % ('training_data/' + str(sys.argv[1]) + '/' + str(self.counter) + '.jpg'))
+      cv2.imwrite(full_img_path, cv2.resize(self.frame, (640,640), interpolation=cv2.INTER_AREA))
+      self.get_logger().info("Frame saved: %s" % ('/media/robocats/32GB_SD/' + str(sys.argv[1]) + '/' + str(self.counter) + '.jpg'))
       self.counter += 1
     else:
       self.get_logger().info("Frame lock not aquired")
